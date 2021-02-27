4ac60f8f943b8d9d75485e82a0efe8282d76330e
C#Master336 <C#Master336@users.noreply.github.com>
2021-02-27 10:19:36 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MLDCA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
